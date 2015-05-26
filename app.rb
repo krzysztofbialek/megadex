cd479d73292d87b350aa505822c388cf2e1e600d
krzysztofbialek <krzysztof.bialek@gmail.com>
2015-05-26 13:12:15 +0200

    working app

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require "net/http"
+require 'uri'
+require 'byebug'
+
+MEGADEX_URL = URI('http://www.galeriasmaku.com.pl/zoliborz/admin/get.php')
+DAYS = {1 => 'pon', 2 => 'wt', 3 => 'sr', 4 => 'czw', 5 => 'pi'}
+
+before do
+  content_type :html, 'charset' => 'utf-8'
+end
+
+get '/hi' do
+  get_lunch
+end
+
+def get_lunch
+  res = Net::HTTP.get(MEGADEX_URL)
+  menu = retrieve_data(res)
+  menu.to_s
+end
+
+def retrieve_data(res)
+  day = DAYS[Time.now.wday]
+  hash = prepare_hash(res)
+  menu_for_day = hash.select {|k,v| k =~ /^#{day}_/} 
+end
+
+def prepare_hash(h)
+  Hash[h.split('&').map{ |s| s.split('=')}]
+end
